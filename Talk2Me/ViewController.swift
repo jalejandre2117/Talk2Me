8add96839edd9cc255c8a8f68d81284d646c03d1
Jorge Alejandre <jorge.alejandre2117@gmail.com>
2018-12-09 20:20:32 -0500

    Initial Commit

---
+++ Talk2Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Talk2Me
+//
+//  Created by Jorge Alejandre on 12/9/18.
+//  Copyright © 2018 Jorge Alejandre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
